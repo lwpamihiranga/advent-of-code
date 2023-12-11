d453db6fe24c0c9ae5d4e35952f963b4638f1aee
Amith Mihiranga <lwpamihiranga@gmail.com>
2023-12-11 10:19:59 +0530

    code cleanup for day4 part1

--- 2023/day4/part1/day4_part1.go
+++ 2023/day4/part1/day4_part1.go
@@ -25,51 +25,24 @@ func main() {
 	var number_match_count_splice []int
 
 	for _, card_line := range file_lines {
-		// fmt.Println(card_line)
-
 		card_line_split := strings.Split(card_line, ":")
-
-		// card_number_string := card_line_split[0]
-		// card_number_string, _ = strings.CutPrefix(card_number_string, "Card")
-		// card_number_string = strings.TrimSpace(card_number_string)
-
-		// fmt.Printf("%+v\n", card_number_string)
 
 		winning_number_string := strings.Split(card_line_split[1], "|")[0]
 		winning_number_string = strings.TrimSpace(winning_number_string)
 
-		// fmt.Println(winning_number_string)
-
 		available_number_string := strings.Split(card_line_split[1], "|")[1]
 		available_number_string = strings.TrimSpace(available_number_string)
-
-		// fmt.Println(available_number_string)
-
-		// winning_number_slice := strings.Split(winning_number_string, " ")
-		// available_number_slice := strings.Split(available_number_string, " ")
 
 		winning_number_slice := strings.Fields(winning_number_string)
 		available_number_slice := strings.Fields(available_number_string)
 
-		// fmt.Println(winning_number_slice)
-		// fmt.Println(available_number_slice)
-
 		number_match_count := -1
 		for _, winning_number := range winning_number_slice {
-			// w_number, _ := strconv.Atoi(strings.TrimSpace(winning_number))
 			for _, available_number := range available_number_slice {
 				if strings.TrimSpace(available_number) == strings.TrimSpace(winning_number) {
 					number_match_count += 1
 					break
 				}
-
-				// a_number, _ := strconv.Atoi(strings.TrimSpace(available_number))
-
-				// if w_number == a_number {
-				// 	fmt.Println(w_number, ":", a_number)
-				// 	number_match_count += 1
-				// 	break
-				// }
 			}
 		}
 
@@ -77,8 +50,6 @@ func main() {
 			number_match_count_splice = append(number_match_count_splice, number_match_count)
 		}
 	}
-
-	// fmt.Println(number_match_count_splice)
 
 	var answer float64
 
